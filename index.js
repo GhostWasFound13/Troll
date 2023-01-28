f0f49fceb2cb50946c7dd9cd8cf6485c4436fe6f
Ghost <87599595+ghostLuna75@users.noreply.github.com>
2023-01-28 07:55:14 +0000

    neW

--- index.js
+++ index.js
@@ -18,3 +18,43 @@ database: {
     tables: ["main"]
   }
 })
+
+ bot.onInteractionCreate()
+bot.readyCommand({
+  channel: "$getVar[channelstatus]",
+  $if: "v4",
+  code: `$log[#new tommy bot made by ShadowYtLol
+  $userTag[$clientID] has logging to discord]
+  $log[command count:$commandsCount
+ uptime: $uptime
+ping: $pingms
+cpu $cpu/100]`,
+}) 
+const PanelObject = new Panel({
+	username: "Ghostwasfound",
+	password: process.env.password,
+	secret: "aoijs", // Just write whatever the fuck you want <3
+	port: 3000,
+	bot,
+	mainFile: "index.js",
+	commands: "Commands"
+});
+
+// Panel events
+PanelObject.loadPanel();
+PanelObject.onError();
+bot.variables(require(`./variables.js`))
+
+require('./handler/status')(bot)
+bot.onJoin()
+bot.onMessage()
+bot.onInteractionCreate()
+bot.onMessageDelete();
+
+
+const loader = new Aoijs.LoadCommands(bot);
+(async () => {
+await loader.load(bot.cmd, "./Commands/")
+})()
+
+loader.setColors( loader.themes.default );
